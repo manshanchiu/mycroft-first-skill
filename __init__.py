f0e3da413eda28dcb26db9bbd986901ad811f06d
RoyChiu <roy.chiu@2359media.com>
2023-03-04 11:06:48 +0800

    test

--- __init__.py
+++ __init__.py
@@ -9,16 +9,16 @@ class RoyFirstSkill(MycroftSkill):
     def handle_skill_first_roy(self, message):
         self.speak_dialog('skill.first.roy')
     
-    @intent_handler('is.there.any.good.intent')
-    def handle_is_there_any_goods(self,message):
-        category_label = message.data.get('category')
-        str = 'yes i find ' + category_label + ' in front of you'
-        # self.speak(str)
-        self.speak_dialog('take.photo')
+    # @intent_handler('is.there.any.good.intent')
+    # def handle_is_there_any_goods(self,message):
+    #     category_label = message.data.get('category')
+    #     str = 'yes i find ' + category_label + ' in front of you'
+    #     # self.speak(str)
+    #     self.speak_dialog('take.photo')
 
-    @intent_handler(IntentBuilder('AskItemBrand').require('brand').build())
-    def handle_ask_item_brand(self,message):
-        self.speak('I am talking about the brand of the item')
+    # @intent_handler(IntentBuilder('AskItemBrand').require('brand').build())
+    # def handle_ask_item_brand(self,message):
+    #     self.speak('I am talking about the brand of the item')
 
 
 def create_skill():
